e50fc8635d2225c3b214a48fc46598b54f20de12
Daniil Golchenko <dagouldur01@gmail.com>
2021-05-02 01:50:45 +0200

    bite

---
+++ app/src/main/java/com/example/nvgup/Home.java
@@ -0,0 +1,38 @@
+package com.example.nvgup;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+    private TextView mTexteBras;
+    private TextView mTexteJambes;
+    private Button mButtonBras;
+    private Button mButtonJambes;
+    private ImageView mImageBras;
+    private ImageView mImageJambes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.fragment_home);
+
+        mTexteBras=findViewById(R.id.texte_bras);
+        mTexteJambes=findViewById(R.id.texte_jambes);
+        mButtonBras=findViewById(R.id.button_bras);
+        mButtonJambes=findViewById(R.id.button_jambes);
+        mImageBras=findViewById(R.id.image_bras);
+        mImageJambes=findViewById(R.id.image_jambes);
+
+        mButtonBras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Mouvements.class );
+                startActivity(intent);
+            }
+        });
+}}
